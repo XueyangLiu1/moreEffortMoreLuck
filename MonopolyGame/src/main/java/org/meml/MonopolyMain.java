79d7f3a9912cb37e681cd87c590cf2474bc74599
Xueyang Liu <xueyang.liu@qq.com>
2022-08-28 23:19:03 -0700

    init project

---
+++ MonopolyGame/src/main/java/org/meml/MonopolyMain.java
@@ -0,0 +1,13 @@
+package org.meml;
+
+/**
+ * Hello world!
+ *
+ */
+public class MonopolyMain
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ MonopolyGame/src/test/java/org/meml/MonopolyMainTest.java
@@ -0,0 +1,20 @@
+package org.meml;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class MonopolyMainTest
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
